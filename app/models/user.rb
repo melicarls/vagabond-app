4202d9a2a416b6dca906fbf807b1b9b23a57b6e2
melicarls <melissacarlson15@gmail.com>
2016-05-05 12:07:00 -0700

    user validations added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,38 @@ class User < ActiveRecord::Base
 
 
 
+  validates :first_name,
+            presence: true,
+            length: { maximum: 255 }
+
+
+  validates :last_name,
+            presence: true,
+            length: { maximum: 255 }
+
+
+  validates :current_city,
+            presence: true,
+            length: { maximum: 255 }
+
+
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+
+
+  validates :email,
+            presence:   true,
+            format:     { with: VALID_EMAIL_REGEX },
+            uniqueness: true,
+            length:     { maximum: 255 }
+
+
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 255 }
+
+
+
 end
