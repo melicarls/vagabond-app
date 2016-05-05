28106c14c9565ee23c41ebab7e1c89c511e2a381
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-05 15:51:46 -0700

    added validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+
+  belongs_to :user
+
+  validates :title,
+            presence: true,
+            length: { maximum: 200 }
+  validates :content,
+            presence: true,
+            length: { maximum: 50000 }
 end
