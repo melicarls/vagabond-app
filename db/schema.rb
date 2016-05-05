7377fcb3aca5dfc27ce15197d8f2d4d6ad0cc91f
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-05 16:03:31 -0700

    migration to create posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505180809) do
+ActiveRecord::Schema.define(version: 20160505224409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
