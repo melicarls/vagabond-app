3251d9cb5c7e6f606e9f62bbfc156e79211c99cb
melicarls <melissacarlson15@gmail.com>
2016-05-06 14:32:19 -0700

    completed routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,38 @@
 Rails.application.routes.draw do
 
+  root to: 'cities#index'
 
-  root to: 'users#index'
-
-#User routes
-
+# User routes
   get '/users', to: 'users#index', as: 'users'
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
   patch '/users/:id', to: 'users#update'
+  #Route to deactivate account
 
 
 # Sessions routes for login, signup, and logout
-
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
   post '/sessions', to: 'sessions#create'
 
-# Post routes
-
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_user_post'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-
-# Cities route
+# Cities routes
   get '/cities/', to: 'cities#index', as: 'cities'
+  get '/cities/:id', to: 'cities#show', as: 'city' # Needs controller action
   get '/cities/new', to: 'cities#new', as: 'new_city'
   post '/cities/', to: 'cities#create'
   get '/cities/edit', to: 'cities#edit', as: 'edit_city'
   patch '/cities/:id', to: 'cities#update'
+  delete '/cities/:id', to: 'cities#destroy'
 
-# Cities_posts routes
 
-resources :cities do
-  resources :posts
-end
+# Posts routes
+get '/cities/:city_id/posts/:id', to: 'posts#show', as: 'post' # Needs controller action
+get '/cities/:city_id/posts/new', to: 'posts#new', as: 'new_post' # Needs controller action
+post '/cities/:city_id/posts', to: 'posts#create' # Needs controller action
+get '/cities/:city_id/posts/:id/edit', to: 'posts#edit', as: 'edit_post' # Needs controller action
+patch '/cities/:city_id/posts/:id', to: 'posts#update' # Needs controller action
+delete '/cities/:city_id/posts/:id', to: 'posts#destroy' # Needs controller action
 
 end
