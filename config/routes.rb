7c4ae2656dabdf0dcd30f0960d8d92c6518d22e9
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-06 14:50:08 -0700

    undo change

--- config/routes.rb
+++ config/routes.rb
@@ -24,15 +24,15 @@ Rails.application.routes.draw do
   post '/cities/', to: 'cities#create'
   get '/cities/edit', to: 'cities#edit', as: 'edit_city'
   patch '/cities/:id', to: 'cities#update'
-  delete '/cities/:id', to: 'cities#destroy'
+  delete '/cities/:id', to: 'cities#destroy' 
 
 
-# Posts routes
-get '/cities/:city_id/posts/:id', to: 'posts#show', as: 'post' # Needs controller action
-get '/cities/:city_id/posts/new', to: 'posts#new', as: 'new_post' # Needs controller action
-post '/cities/:city_id/posts', to: 'posts#create' # Needs controller action
-get '/cities/:city_id/posts/:id/edit', to: 'posts#edit', as: 'edit_post' # Needs controller action
-patch '/cities/:city_id/posts/:id', to: 'posts#update' # Needs controller action
-delete '/cities/:city_id/posts/:id', to: 'posts#destroy' # Needs controller action
+  # Posts routes
+  get '/cities/:city_id/posts/:id', to: 'posts#show', as: 'post' # Needs controller action
+  get '/cities/:city_id/posts/new', to: 'posts#new', as: 'new_post' # Needs controller action
+  post '/cities/:city_id/posts', to: 'posts#create' # Needs controller action
+  get '/cities/:city_id/posts/:id/edit', to: 'posts#edit', as: 'edit_post' # Needs controller action
+  patch '/cities/:city_id/posts/:id', to: 'posts#update' # Needs controller action
+  delete '/cities/:city_id/posts/:id', to: 'posts#destroy' # Needs controller action
 
 end
