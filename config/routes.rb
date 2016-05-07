b271a4ceab518d5d0c323c46f8887895efaa95de
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-06 18:44:02 -0700

    switched to resources

--- config/routes.rb
+++ config/routes.rb
@@ -17,23 +17,25 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   post '/sessions', to: 'sessions#create'
 
-# Cities routes
-  get '/cities/', to: 'cities#index', as: 'cities'
-  get '/cities/:id', to: 'cities#show', as: 'city' # Needs controller action
-  get '/cities/new', to: 'cities#new', as: 'new_city'
-  post '/cities/', to: 'cities#create'
-  get '/cities/edit', to: 'cities#edit', as: 'edit_city'
-  patch '/cities/:id', to: 'cities#update'
-  delete '/cities/:id', to: 'cities#destroy' # Needs controller action, might need dependent destroy
+  resources :cities do
+    resources :posts
+  end
+#   city_posts GET    /cities/:city_id/posts(.:format)          posts#index
+#              POST   /cities/:city_id/posts(.:format)          posts#create
+# new_city_post GET    /cities/:city_id/posts/new(.:format)      posts#new
+# edit_city_post GET    /cities/:city_id/posts/:id/edit(.:format) posts#edit
+#    city_post GET    /cities/:city_id/posts/:id(.:format)      posts#show
+#              PATCH  /cities/:city_id/posts/:id(.:format)      posts#update
+#              PUT    /cities/:city_id/posts/:id(.:format)      posts#update
+#              DELETE /cities/:city_id/posts/:id(.:format)      posts#destroy
+#       cities GET    /cities(.:format)                         cities#index
+#              POST   /cities(.:format)                         cities#create
+#     new_city GET    /cities/new(.:format)                     cities#new
+#    edit_city GET    /cities/:id/edit(.:format)                cities#edit
+#         city GET    /cities/:id(.:format)                     cities#show
+#              PATCH  /cities/:id(.:format)                     cities#update
+#              PUT    /cities/:id(.:format)                     cities#update
+#              DELETE /cities/:id(.:format)                     cities#destroy
 
-
-  # Posts routes
-  get '/cities/:city_id/posts', to: "posts#index", as: 'posts'
-  get '/cities/:city_id/posts/new', to: 'posts#new', as: 'new_post' # Needs controller action
-  post '/cities/:city_id/posts', to: 'posts#create' # Needs controller action
-  get '/cities/:city_id/posts/:id', to: 'posts#show', as: 'post' # Needs controller action
-  get '/cities/:city_id/posts/:id/edit', to: 'posts#edit', as: 'edit_post' # Needs controller action
-  patch '/cities/:city_id/posts/:id', to: 'posts#update' # Needs controller action
-  delete '/cities/:city_id/posts/:id', to: 'posts#destroy' # Needs controller action
 
 end
