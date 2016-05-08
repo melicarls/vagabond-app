da3f2988c063fb93e8045f3cfed6499162e6a77d
melicarls <melissacarlson15@gmail.com>
2016-05-08 16:13:09 -0700

    Data validation- city name and image url are required and must be unique

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,11 @@
 class City < ActiveRecord::Base
   has_many :posts
+
+  validates :name,
+            presence: true,
+            uniqueness: true
+
+  validates :photo,
+            presence: true,
+            uniqueness: true
 end
