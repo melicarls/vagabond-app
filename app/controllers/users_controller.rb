7ce23bcd4f036138b2d2a3af822ac9fe4921c875
melicarls <melissacarlson15@gmail.com>
2016-05-08 11:48:22 -0700

    Removed users#show route and action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-def index
-  @users = User.all
-  render :index
-end
-
 
 def new
   @user = User.new
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'cities#index'
 
 # User routes
-  get '/users', to: 'users#index', as: 'users'
+
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
