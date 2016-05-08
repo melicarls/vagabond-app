2cecb2bd10805b3595ec98af267e196de06e9e46
hectorcoronado <coronado.hector@gmail.com>
2016-05-08 13:12:00 -0700

    user show route protected

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,38 @@ end
 
 def show
   @user = User.find_by_id(params[:id])
-  render :show
+  # render :show
+  if session[:user_id] != @user[:id]
+    flash[:notice] = "You may only view your own profile page."
+    redirect_to login_path
+  else
+    render :show
+  end
+
 end
+
+# def edit
+#   return if inactive_redirect
+#   post_id = params[:id]
+#   @post = Post.find_by(id: post_id)
+#   city_id = params[:city_id]
+#   @city = City.find_by(id: city_id)
+#   if session[:user_id] != @post.user_id
+#     redirect_to login_path
+#   end
+# end
+
+
+
+# def show
+#   return if inactive_redirect
+#   post_id = params[:id]
+#   @post = Post.find_by(id: post_id)
+#   city_id = params[:city_id]
+#   @city = City.find_by(id: city_id)
+# end
+
+
 
 def edit
   @user = User.find_by_id(params[:id])
@@ -74,3 +104,50 @@ end
 
 
 end
+
+
+
+ ##############################
+ # EXAMPLE
+ ##############################
+
+
+
+
+  # def update
+  #   return if inactive_redirect
+  #   city = City.find(params[:city_id])
+  #   post_id = params[:id]
+  #   @post = Post.find_by(id: post_id)
+  #   user_id = current_user[:id]
+  #   @post[:user_id] = user_id
+  #   if session[:user_id] == @post.user_id
+  #     @post.update(post_params)
+  #     flash[:notice] = "Post successfully updated!"
+  #     redirect_to city_path(city)
+  #   else
+  #     flash[:error] = @post.errors.full_messages.join(", ")
+  #     redirect_to edit_post_path
+  #   end
+  # end
+  #
+  # def destroy
+  #   return if inactive_redirect
+  #   post_id = params[:id]
+  #   post = Post.find_by(id: post_id)
+  #
+  #
+  #   if session[:user_id] == post.user_id
+  #     post.destroy
+  #     city_id = params[:city_id]
+  #     city = City.find_by(id: city_id)
+  #     redirect_to city_path(city)
+  #   else
+  #     flash[:notice] = "You may only delete your own posts."
+  #     redirect_to login_path
+  #   end
+  # end
+
+#####################
+# END
+####################
