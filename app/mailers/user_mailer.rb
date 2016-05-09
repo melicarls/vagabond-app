43273257c3f2775571bc65a9a62b5951de0970f7
hectorcoronado <coronado.hector@gmail.com>
2016-05-09 08:15:40 -0700

    mailer works

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,12 @@
 class UserMailer < ApplicationMailer
 
-  default from "vagabondgroup2@gmail.com"
+  default from: "vagabondgroup2@gmail.com"
 
   def welcome_email(user)
+
     @user = user
     @url = "https://glacial-ridge-30784.herokuapp.com/"
-    mail(to: @user.email, subject: 'Welcome to Vagabond')
+    mail(to: @user.email , subject: 'Welcome to Vagabond')
   end
 
 end
